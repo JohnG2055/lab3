cda6b8b4c41f6bd0dba442fc7910bc49ba0b2367
JohnG2055 <JohnG2055@users.noreply.github.com>
2021-01-20 15:41:23 -0800

    finish lab3 and ready for heroku deployment

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -3,6 +3,7 @@
 // Call this function when the page loads (the "ready" event)
 $(document).ready(function() {
 	initializePage();
+	projectClick();
 })
 
 /*
@@ -11,8 +12,22 @@ $(document).ready(function() {
 function initializePage() {
 	$("#testjs").click(function(e) {
 		$('.jumbotron h1').text("Javascript is connected");
+		$(this).text("Too many questions is the Chan disease.");
+		$(".jumbotron p").toggleClass("active");
 	});
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+	$("a.thumbnail").click(projectClick);
+}
+
+function projectClick(e) {
+	e.preventDefault();
+    var containingProject = $(this).closest(".project");
+    var description = $(containingProject).find(".project-description");
+    if (description.length == 0) {
+       $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+    } else {
+       description.html("<p>Stop clicking on me! You just did it at " + (new Date()) + "</p>");
+    }
 }
